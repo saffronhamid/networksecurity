dfdc42b8966c252a9c929ec557bcc0804f5c9d01
Faizan Hamid Lone <90542362+saffronhamid@users.noreply.github.com>
2025-09-26 00:43:09 +0200

    update setup

--- setup.py
+++ setup.py
@@ -32,8 +32,8 @@ def get_requirements()->List[str]:
 setup(
     name="NetworkSecurity",
     version="0.0.1",
-    author="Krish Naik",
-    author_email="krishnaik06@gmail.com",
+    author="faizan hamid",
+    author_email="saffronhamid1234@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements()
 )
