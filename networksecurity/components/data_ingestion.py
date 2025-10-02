1d3cd206a0782867289ff7f93aed844b877c16b0
Faizan Hamid Lone <90542362+saffronhamid@users.noreply.github.com>
2025-10-02 22:44:14 +0200

    update ingestion

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -7,7 +7,6 @@ from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.entity.artifact_entity import DataIngestionArtifact
 import os
-import os
 import sys
 import numpy as np
 import pandas as pd
