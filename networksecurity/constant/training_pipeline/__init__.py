f5fbae14fd3ea3ce9841f2d677c7db0a66795743
Faizan Hamid Lone <90542362+saffronhamid@users.noreply.github.com>
2025-11-18 12:20:57 +0100

    update

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -55,6 +55,7 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
     "n_neighbors": 3,
     "weights": "uniform",
 }
+
 DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
 
 DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
