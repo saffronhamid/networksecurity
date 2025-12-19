06986e26aecab56967f61cb4db9549749c6d42a0
Faizan Hamid Lone <90542362+saffronhamid@users.noreply.github.com>
2025-12-19 21:09:16 +0100

    update

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -6,7 +6,6 @@ from networksecurity.constant.training_pipeline import SCHEMA_FILE_PATH
 from scipy.stats import ks_2samp
 import pandas as pd
 import os,sys
-import os
 from networksecurity.utils.main_utils.utils import read_yaml_file,write_yaml_file
 
 class DataValidation:
